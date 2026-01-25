be753f2bca179d3f41d170eebd2f3768abe5d939
stelios <smacricostas@gmail.com>
2026-01-25 15:49:10 +0200

    report update

--- BuddyFitAppScriptsV1.0.ps1
+++ BuddyFitAppScriptsV1.0.ps1
@@ -20,6 +20,8 @@
 & C://Python313/python.exe "C:/smakrykoDev/GitHubRepos/MS-Buddy-Fitness-App/Apex-RunAnalysis/scripts/RunningAnalysis_v60.py"
 
 & C://Python313/python.exe "C:/smakrykoDev/GitHubRepos/MS-Buddy-Fitness-App/Apex-RunAnalysis/scripts/RunningAnalysis_v6.5.py"
+
+& C://Python313/python.exe "C:/smakrykoDev/GitHubRepos/MS-Buddy-Fitness-App/Apex-RunAnalysis/scripts/RunningAnalysis_v6.26 .py"
     #interactive DashBoard - Run Analysis
    
     streamlit run scripts/app.py
