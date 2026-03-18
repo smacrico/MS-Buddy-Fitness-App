d0615a1f466bcf24dfe5f1e7dd342fbb6512f30a
stelios <smacricostas@gmail.com>
2026-03-18 19:16:48 +0200

    scripts update and powerBi report

--- BuddyFitAppScriptsV1.0.ps1
+++ BuddyFitAppScriptsV1.0.ps1
@@ -9,13 +9,22 @@
 & C:/Python313/python.exe C:/Users/djsco/AppData/Roaming/Python/Python313/Scripts/garmindb_cli.py --all --download --import --analyze --latest
 
 & C:/Python313/python.exe "C:/smakrykoDev/GitHub_dls/MS-Buddy-Fitness-App/utilities-tools/jHeel_plugin v6.26.py"
+& C:/Python313/python.exe "C:/smakrykoDev/GitHub_dls/MS-Buddy-Fitness-App/Apex-RunAnalysis/scripts/createRunAnalDB - v6.26.py"
+& C:/Python313/python.exe c:/smakrykoDev/GitHub_dls/MS-Buddy-Fitness-App/APEX-RunAnalysis/Dev_Scripts/RunningAnalysis_v6.26-Dev.py
+& C:/Python313/python.exe c:/smakrykoDev/GitHub_dls/MS-Buddy-Fitness-App/Mercury-HRVAnalysis/Dev_Scripts/HRV_Analytics_v6.25-DEV.py
 
+    #interactive DashBoard - Run Analysis
+   
+    streamlit run scripts/app.py
+
+    #interactive DashBoard - HRV data analysis
+    streamlit run hrv_streamlit_dashboard.py
+
+####################################3
 
 ## Run Analysis
 & C:/Python313/python.exe "C:/smakrykoDev/GitHub_dls/MS-Buddy-Fitness-App/Apex-RunAnalysis/scripts/createRunAnalDB.py"
 & C:/Python313/python.exe "C:/smakrykoDev/GitHub_dls/MS-Buddy-Fitness-App/Apex-RunAnalysis/scripts/createRunAnalDB - v6.26.py"
-
-
 
 
 
@@ -38,6 +47,10 @@
     #interactive DashBoard - Run Analysis
    
     streamlit run scripts/app.py
+
+    #interactive DashBoard - HRV data analysis
+    streamlit run hrv_streamlit_dashboard.py
+
 
 ## HRV Analysis
     ## HRV Data Warehouse (v1.0 & v2.0)
